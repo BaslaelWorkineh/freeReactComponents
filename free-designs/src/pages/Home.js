407fdd3cc50a59e3f972bbb24ac37655d02d74e2
Baslael Workineh <baslaworku@gmail.com>
2024-03-14 17:02:57 +0300

    side bar

--- free-designs/src/pages/Home.js
+++ free-designs/src/pages/Home.js
@@ -1,31 +1,45 @@
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
 function Home() {
+  const menuItems = [
+    'Buttons',
+    'Cards',
+    'Carousel',
+    'Dropdown',
+    'Form',
+    'Gallery',
+    'Modal',
+    'Navbar',
+    'Pagination',
+    'Tooltip',
+    'Card deck',
+    'Notification',
+    'Progress bar',
+    'Slider',
+    'Tag',
+    'Tabs',
+    'Toggle switch'
+  ];
+
   return (
     <div className='body'>
-         <Sidebar >
-        <Menu className='sidebarWrapper' >
-        <MenuItem className='sidebar'>Buttons</MenuItem>
-        <MenuItem className='sidebar'>Cards</MenuItem>
-        <MenuItem className='sidebar'>Carousel</MenuItem>
-        <MenuItem className='sidebar'>Dropdown</MenuItem>
-        <MenuItem className='sidebar'>Form</MenuItem>
-        <MenuItem className='sidebar'>Gallery</MenuItem>
-        <MenuItem className='sidebar'>Modal</MenuItem>
-        <MenuItem className='sidebar'>Navbar</MenuItem>
-        <MenuItem className='sidebar'>Pagination</MenuItem>
-        <MenuItem className='sidebar'>Tooltip</MenuItem>
-        <MenuItem className='sidebar'>Card deck</MenuItem>
-        <MenuItem className='sidebar'>Notification</MenuItem>
-        <MenuItem className='sidebar'>Progress bar</MenuItem>
-        <MenuItem className='sidebar'>Slider</MenuItem>
-        <MenuItem className='sidebar'>Tag</MenuItem>
-        <MenuItem className='sidebar'>Tabs</MenuItem>
-        <MenuItem className='sidebar'>Toggle switch</MenuItem>
-      </Menu>
-    </Sidebar>
+      <Sidebar>
+        <Menu className='sidebarWrapper'>
+          {menuItems.map((label, index) => (
+            <MenuItem key={index} className='sidebar'>
+              {label}
+            </MenuItem>
+          ))}
+          <SubMenu label="Components" className='sidebar'>
+            {menuItems.slice(2).map((label, index) => (
+              <MenuItem key={index} className='sidebar'>
+                {label}
+              </MenuItem>
+            ))}
+          </SubMenu>
+        </Menu>
+      </Sidebar>
     </div>
-   
   );
 }
 
